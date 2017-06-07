6a087edc65af537ba37f99797d9efe5bc86040fb
Eliseu Daroit <eliseudaroitjr@gmail.com>
2017-06-06 22:32:15 -0300

    adding views

---
+++ client/js/app-es6/views/DealsView.js
@@ -0,0 +1,45 @@
+import {View} from './View';
+import {currentInstance} from '../controllers/DealController';
+import {DateConverter} from '../helpers/DateConverter';
+
+export class DealsView extends View {
+
+  constructor(element) {
+    super(element);
+
+    element.addEventListener('click', function(event) {
+      if(event.target.nodeName == 'TH')
+        currentInstance().order(event.target.textContent.toLowerCase());
+    });
+  }
+
+  template(model) {
+    return `
+    <table class="table table-hover table-bordered">
+      <thead>
+        <tr>
+          <th>DATA</th>
+          <th>QUANTIDADE</th>
+          <th>VALOR</th>
+          <th>VOLUME</th>
+        </tr>
+      </thead>
+      <tbody>
+      ${model.deals.map(deal => `
+        <tr>
+          <td>${DateConverter.dateToText(deal.date)}</td>
+          <td>${deal.amount}</td>
+          <td>${deal.value}</td>
+          <td>${deal.volume}</td>
+        </tr>
+        `).join('')}
+      </tbody>
+      <tfoot>
+        <td colspan="3"></td>
+        <td>${model.totalVolume}</td>
+      </tfoot>
+    </table>
+      `;
+  }
+
+}
