ed12c058fdb8c10861980e4b6b9bd847aa65264e
Eliseu Daroit <eliseudaroitjr@gmail.com>
2017-06-06 22:33:09 -0300

    adding helpers

---
+++ client/js/app-es6/helpers/DateConverter.js
@@ -0,0 +1,18 @@
+export class DateConverter {
+
+  constructor() {
+    throw new Error('Esta classe não pode ser instanciada');
+  }
+
+  static dateToText(date) {
+    return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+  }
+
+  static textToDate(text) {
+    if(!/\d{2}\/\d{2}\/\d{4}/.test(text))
+      throw new Error('Deve estar no formato dd/mm/aaaa');
+
+    return new Date(...text.split('/').reverse().map((item, index) => item - index % 2));
+  }
+
+}
