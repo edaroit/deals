896a579330f0cfcc98998bc83652ad05e708cb1a
Eliseu Daroit <eliseudaroitjr@gmail.com>
2017-06-06 22:32:57 -0300

    adding models

---
+++ client/js/app-es6/models/Deal.js
@@ -0,0 +1,26 @@
+export class Deal {
+
+  constructor(date, amount, value) {
+    this._date = new Date(date.getTime());
+    this._amount = amount;
+    this._value = value;
+    Object.freeze(this);
+  }
+
+  get date() {
+    return new Date(this._date.getTime());
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  get volume() {
+    return this._amount * this._value;
+  }
+
+}
---
+++ client/js/app-es6/models/DealsList.js
@@ -0,0 +1,31 @@
+export class DealsList {
+
+  constructor() {
+    this._deals = [];
+  }
+
+  get deals() {
+    return [].concat(this._deals);
+  }
+
+  get totalVolume() {
+   return this._deals.reduce((total, deal) => total + deal.volume, 0.0);
+ }
+
+  add(deal) {
+    this._deals.push(deal);
+  }
+
+  empty() {
+    this._deals = [];
+  }
+
+  order(column) {
+    this._deals.sort(column);
+  }
+
+  reverseOrder() {
+    this._deals.reverse();
+  }
+
+}
