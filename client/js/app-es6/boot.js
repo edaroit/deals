9bf1c76d8052a5faaa3884c22887650fb5269c3c
Eliseu Daroit <eliseudaroitjr@gmail.com>
2017-12-09 15:46:56 -0200

    Improve syntax on boot js

--- client/js/app-es6/boot.js
+++ client/js/app-es6/boot.js
@@ -1,9 +1,7 @@
-import {currentInstance} from './controllers/DealController'
-import {} from './polyfill/fetch'
-
-let dealController = currentInstance()
+import { currentInstance } from './controllers/DealController'
+import from './polyfill/fetch'
+const dealController = currentInstance()
 
 document.querySelector('.form').onsubmit = dealController.add.bind(dealController)
 document.querySelector('[type=button]').onclick = dealController.delete.bind(dealController)
-
 document.querySelector('#post').onsubmit = dealController.post.bind(dealController)
