e248be0202d3b135ef43b9246edcbe30953c7d8a
Eliseu Daroit <eliseudaroitjr@gmail.com>
2017-12-09 15:27:07 -0200

    Improve syntax on api

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,15 +1,8 @@
-var api = require('../api');
+const api = require('../api')
 
-module.exports  = function(app) {
-  app.route('/deal/week')
-  .get(api.weekList);
-
-  app.route('/deal/last')
-  .get(api.lastWeekList);
-
-  app.route('/deal/fortnight')
-  .get(api.fortnightWeekList);
-
-  app.route('/deal')
-  .post(api.registerDeal);
-};
+module.exports  = app => {
+  app.route('/deal/week').get(api.weekList)
+  app.route('/deal/last').get(api.lastWeekList)
+  app.route('/deal/fortnight').get(api.fortnightWeekList)
+  app.route('/deal').post(api.registerDeal)
+}
