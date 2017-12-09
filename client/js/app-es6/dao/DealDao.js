69853324e8af669572f97004e53ee242df65a61e
Eliseu Daroit <eliseudaroitjr@gmail.com>
2017-12-09 15:09:17 -0200

    Change syntax inside DealDao

--- client/js/app-es6/dao/DealDao.js
+++ client/js/app-es6/dao/DealDao.js
@@ -1,4 +1,4 @@
-import {Deal} from '../models/Deal'
+import { Deal } from '../models/Deal'
 
 export class DealDao {
   constructor (connection) {
@@ -8,63 +8,45 @@ export class DealDao {
 
   add (deal) {
     return new Promise((resolve, reject) => {
-      let request = this._connection
-      .transaction([this._store], 'readwrite')
-      .objectStore(this._store)
-      .add(deal)
-
-      request.onsuccess = e => {
-        resolve()
-      }
-
-      request.onerror = e => {
-        console.log(e.target.error)
-        reject('Não foi possível adicionar a negociação')
-      }
+      const request = this._connection
+        .transaction([this._store], 'readwrite')
+        .objectStore(this._store)
+        .add(deal)
+      request.onsuccess = e => resolve()
+      request.onerror = e => reject('Não foi possível adicionar a negociação')
     })
   }
 
   delete () {
     return new Promise((resolve, reject) => {
-      let request = this._connection
-      .transaction([this._store], 'readwrite')
-      .objectStore(this._store)
-      .clear()
-
+      const request = this._connection
+        .transaction([this._store], 'readwrite')
+        .objectStore(this._store)
+        .clear()
       request.onsuccess = e => resolve('Negociações apagadas com sucesso')
-
-      request.onerror = e => {
-        console.log(e.target.error)
-        reject('Não foi possível apagar as negociações')
-      }
+      request.onerror = e => reject('Não foi possível apagar as negociações')
     })
   }
 
   list () {
     return new Promise((resolve, reject) => {
-      let cursor = this._connection
-      .transaction([this._store], 'readwrite')
-      .objectStore(this._store)
-      .openCursor()
+      const cursor = this._connection
+        .transaction([this._store], 'readwrite')
+        .objectStore(this._store)
+        .openCursor()
 
-      let deals = []
-
+      const deals = []
       cursor.onsuccess = e => {
-        let actual = e.target.result
-
+        const actual = e.target.result
         if (actual) {
-          let data = actual.value
+          const data = actual.value
           deals.push(new Deal(data._date, data._amount, data._value))
           actual.continue()
         } else {
           resolve(deals)
         }
       }
-
-      cursor.onerror = e => {
-        console.log(e.target.error)
-        reject('Não foi possível listar as negociações')
-      }
+      cursor.onerror = e => reject('Não foi possível listar as negociações')
     })
   }
 }
